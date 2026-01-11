c164116f41225f7f38adcdca90d4e2fa612bc37c
RAMISK <rkomiakov@mail.ru>
2026-01-12 00:07:20 +0300

    Урок 3. Автотест (паттерн PageObject)
    - изменил и добавил новые локаторы
    - Повторил код из урока
    - Добаил проверку текста "Введите пароль "на странице входа

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -3,21 +3,56 @@ from selenium.webdriver.common.by import By
 
 
 class LoginPageLocators:
-    BUTTON_Enter = (By.XPATH, "//a[@title='Вход']")
-    BUTTON_Enter_QR = (By.XPATH, "//a[@title='QR-код']")
+    BUTTON_ENTER = (By.XPATH, "//a[@title='Вход']")
+    BUTTON_ENTER_QR = (By.XPATH, "//a[@title='QR-код']")
 
     LOGIN_FIELD = (By.ID, 'field_email')
     PASSWORD_FIELD = (By.ID, 'field_password')
 
-    LOGIN_BUTTON_Enter = (By.XPATH, "//button[contains(.,'Войти')]")
-    LOGIN_BUTTON_Enter_QR = (By.XPATH, "//button[contains(.,'Войти по QR-коду')]")
-    BUTTON_NO_Enter = (By.XPATH, "// button[text() = 'Не получается войти?']")
+    LOGIN_BUTTON_ENTER = (
+        By.XPATH, "//button[contains(@class, 'vkuiButton__host')][@type='submit'][contains(.,'Войти')]")
+    LOGIN_BUTTON_ENTER_QR = (By.XPATH, "//button[contains(.,'Войти по QR-коду')]")
+    BUTTON_NO_ENTER = (By.XPATH, "// button[text() = 'Не получается войти?']")
 
-    BUTTON_REGISTRATION = (By.XPATH, "//button[@type='button' and .//span/span[text()='Зарегистрироваться']")
+    BUTTON_REGISTRATION = (By.XPATH, "//button[@type='button' and .//*[contains(text(), 'Зарегистрироваться')]]")
     BUTTON_VK = (By.XPATH, "//a[contains(@class, '__vk_id')]")
-    BUTTON_Mail = (By.XPATH, "//a[contains(@class, '__mailru')]']")
-    BUTTON_Yandex = (By.XPATH, "//a[contains(@class, '__yandex')]']")
+    BUTTON_Mail = (By.XPATH, "//a[contains(@class, '__mailru')]")
+    BUTTON_Yandex = (By.XPATH, "//a[contains(@class, '__yandex')]")
+    ERROR_TEXT_LOGIN = (By.XPATH, "//span[.='Введите логин']")
+    ERROR_TEXT_PASSWORD = (By.XPATH, "//span[.='Введите пароль']")
 
 
 class LoginPageHelper(BasePage):
-    pass
+    def __init__(self, driver):
+        self.driver = driver
+        self.check_page()
+
+    def check_page(self):
+        self.find_element(LoginPageLocators.BUTTON_ENTER)
+        self.find_element(LoginPageLocators.BUTTON_ENTER_QR)
+        self.find_element(LoginPageLocators.LOGIN_FIELD)
+        self.find_element(LoginPageLocators.PASSWORD_FIELD)
+        self.find_element(LoginPageLocators.LOGIN_BUTTON_ENTER)
+        self.find_element(LoginPageLocators.LOGIN_BUTTON_ENTER_QR)
+        self.find_element(LoginPageLocators.BUTTON_NO_ENTER)
+        self.find_element(LoginPageLocators.BUTTON_REGISTRATION)
+        self.find_element(LoginPageLocators.BUTTON_VK)
+        self.find_element(LoginPageLocators.BUTTON_Mail)
+        self.find_element(LoginPageLocators.BUTTON_Yandex)
+
+    def click_login(self):
+        self.find_element(LoginPageLocators.LOGIN_BUTTON_ENTER).click()
+
+    def send_keys_login_field(self, value):
+        field = self.find_element(LoginPageLocators.LOGIN_FIELD)
+        field.send_keys(value)
+
+    def password_field_empty(self):
+        field = self.find_element(LoginPageLocators.PASSWORD_FIELD)
+        field.clear()
+
+    def get_error_text_login(self):
+        return self.find_element(LoginPageLocators.ERROR_TEXT_LOGIN).text
+
+    def get_error_text_password(self):
+        return self.find_element(LoginPageLocators.ERROR_TEXT_PASSWORD).text
