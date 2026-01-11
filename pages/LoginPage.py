9c6d9b1da5b98b31bdd865ce265edd671c9c254e
RAMISK <rkomiakov@mail.ru>
2026-01-11 21:58:41 +0300

    Исправлен нейминг элементов страницы

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -3,21 +3,44 @@ from selenium.webdriver.common.by import By
 
 
 class LoginPageLocators:
-    BUTTON_Enter = (By.XPATH, "//a[@title='Вход']")
-    BUTTON_Enter_QR = (By.XPATH, "//a[@title='QR-код']")
+    BUTTON_ENTER = (By.XPATH, "//a[@title='Вход']")
+    BUTTON_ENTER_QR = (By.XPATH, "//a[@title='QR-код']")
 
     LOGIN_FIELD = (By.ID, 'field_email')
     PASSWORD_FIELD = (By.ID, 'field_password')
 
-    LOGIN_BUTTON_Enter = (By.XPATH, "//button[contains(.,'Войти')]")
-    LOGIN_BUTTON_Enter_QR = (By.XPATH, "//button[contains(.,'Войти по QR-коду')]")
-    BUTTON_NO_Enter = (By.XPATH, "// button[text() = 'Не получается войти?']")
+    LOGIN_BUTTON = (By.XPATH, "//button[contains(@class, 'vkuiButton__host')][@type='submit'][contains(.,'Войти')]")
+    LOGIN_BUTTON_QR = (By.XPATH, "//button[contains(.,'Войти по QR-коду')]")
+    BUTTON_NO_ENTER = (By.XPATH, "// button[text() = 'Не получается войти?']")
 
-    BUTTON_REGISTRATION = (By.XPATH, "//button[@type='button' and .//span/span[text()='Зарегистрироваться']")
+    BUTTON_REGISTRATION = (By.XPATH, "//button[@type='button' and .//*[contains(text(), 'Зарегистрироваться')]]")
     BUTTON_VK = (By.XPATH, "//a[contains(@class, '__vk_id')]")
-    BUTTON_Mail = (By.XPATH, "//a[contains(@class, '__mailru')]']")
-    BUTTON_Yandex = (By.XPATH, "//a[contains(@class, '__yandex')]']")
+    BUTTON_Mail = (By.XPATH, "//a[contains(@class, '__mailru')]")
+    BUTTON_Yandex = (By.XPATH, "//a[contains(@class, '__yandex')]")
+    ERROR_TEXT = (By.XPATH, "//span[text()='Введите логин']")
 
 
 class LoginPageHelper(BasePage):
-    pass
+    def __init__(self, driver):
+        self.driver = driver
+        self.check_page()
+
+    def check_page(self):
+        self.find_element(LoginPageLocators.BUTTON_ENTER)
+        self.find_element(LoginPageLocators.BUTTON_ENTER_QR)
+        self.find_element(LoginPageLocators.LOGIN_FIELD)
+        self.find_element(LoginPageLocators.PASSWORD_FIELD)
+        self.find_element(LoginPageLocators.LOGIN_BUTTON)
+        self.find_element(LoginPageLocators.LOGIN_BUTTON_QR)
+        self.find_element(LoginPageLocators.BUTTON_NO_ENTER)
+        self.find_element(LoginPageLocators.BUTTON_REGISTRATION)
+        self.find_element(LoginPageLocators.BUTTON_VK)
+        self.find_element(LoginPageLocators.BUTTON_Mail)
+        self.find_element(LoginPageLocators.BUTTON_Yandex)
+
+    def click_login(self):
+        self.find_element(LoginPageLocators.LOGIN_BUTTON).click()
+
+    def get_error_text(self):
+        return self.find_element(LoginPageLocators.ERROR_TEXT).text
+
