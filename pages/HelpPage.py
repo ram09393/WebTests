cdcd288776ef396a25560cd97a50647397721e76
RAMISK <rkomiakov@mail.ru>
2026-01-16 02:27:38 +0300

    Урок 7. Скролл страницы
    - Добавил тест ('Проверяем скролл по элементам на странице ok.ru/help' )
    
    - Добавил тест (Проверка перехода на страницу ok.ru/help/reklamnyi-kabinet)
    
    - Добавил к тестам отчетность allure

---
+++ pages/HelpPage.py
@@ -0,0 +1,50 @@
+from pages.BasePage import BasePage
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+import allure
+
+
+class HelpPageLocators:
+    SEARCH_FIELD = (By.XPATH, '//input[@type="search"]')
+    ACTUAL_TODAY = (By.XPATH, '//a[contains(@href, "segodnya-aktualno")]')
+    REGISTRATION = (By.XPATH, '//a[contains(@href,"registraciya")]')
+    MY_PROFILE = (By.XPATH, '//a[contains(@href,"moi-profil")]')
+    COMMUNICATION = (By.XPATH, '//a[contains(@href,"obshchenie")]')
+    PROFILE_ACCESS = (By.XPATH, '//a[contains(@href,"dostup-k-profilu")]')
+    SECURITY = (By.XPATH, '//a[contains(@href,"bezopasnost")]')
+    GROUPS = (By.XPATH, '//a[contains(@href,"gruppy")]')
+    PAYED_FUNCTIONS = (By.XPATH, '//a[contains(@href,"platnye-funkcii")]')
+    SPAM = (By.XPATH, '//a[contains(@href,"narusheniya-i-spam")]')
+    GAMES_AND_APPS = (By.XPATH, '//a[contains(@href,"igry-i-prilojeniya")]')
+    OTHER_SERVICES = (By.XPATH, '//a[contains(@href,"drugie-servisy")]')
+    USEFUL_INFORMATION = (By.XPATH, '//a[contains(@href,"poleznaya-informaciya")]')
+    MARKETING_CABINET = (By.XPATH, '//a[contains(@href,"reklamnyi-kabinet")]')
+
+
+class HelpPageHelper(BasePage):
+    def __init__(self, driver):
+        self.driver = driver
+        self.check_page()
+
+    def check_page(self):
+        with allure.step('Проверяем корректность загрузки страницы'):
+            self.attach_screenshot()
+        self.find_element(HelpPageLocators.SEARCH_FIELD)
+        self.find_element(HelpPageLocators.ACTUAL_TODAY)
+        self.find_element(HelpPageLocators.REGISTRATION)
+        self.find_element(HelpPageLocators.MY_PROFILE)
+        self.find_element(HelpPageLocators.COMMUNICATION)
+        self.find_element(HelpPageLocators.PROFILE_ACCESS)
+        self.find_element(HelpPageLocators.SECURITY)
+        self.find_element(HelpPageLocators.GROUPS)
+        self.find_element(HelpPageLocators.PAYED_FUNCTIONS)
+        self.find_element(HelpPageLocators.SPAM)
+        self.find_element(HelpPageLocators.GAMES_AND_APPS)
+        self.find_element(HelpPageLocators.OTHER_SERVICES)
+        self.find_element(HelpPageLocators.USEFUL_INFORMATION)
+        self.find_element(HelpPageLocators.MARKETING_CABINET)
+
+    @allure.step('Проверяем скролл по элементам на странице ok.ru/help и переход на стр ok.ru/help/reklamnyi-kabinet')
+    def scrollToitem(self, locator):
+        scroll_item = self.find_element(locator)
+        ActionChains(self.driver).scroll_to_element(scroll_item).click(scroll_item).perform()
