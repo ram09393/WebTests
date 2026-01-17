b3eae0ac9ba63019549abbb4131e6a50dbe73d18
RAMISK <rkomiakov@mail.ru>
2026-01-18 00:13:22 +0300

    Опечатка в названии класса 2 раза HelperHelper

--- tests/test_help.py
+++ tests/test_help.py
@@ -1,7 +1,7 @@
 import allure
 from core.BaseTest import browser
 from pages.BasePage import BasePageHelper
-from pages.HelpPage import HelpPageHelperHelper, HelpPageLocators
+from pages.HelpPage import HelpPageHelper, HelpPageLocators
 from pages.MarketingCabinetHelpPage import MarketingCabinetHelper, MarketingCabinetHelplocators
 
 BASE_URL = 'https://ok.ru/help'
@@ -11,5 +11,5 @@ BASE_URL = 'https://ok.ru/help'
 @allure.title('Проверка элементов на странице и скролла страницы до элемента с кнопкой "Рекламный кабинет ')
 def test_help_test(browser):
     BasePageHelper(browser).get_url(BASE_URL)
-    HelpPage = HelpPageHelperHelper(browser)
+    HelpPage = HelpPageHelper(browser)
     HelpPage.scrollToitem(HelpPageLocators.MARKETING_CABINET)
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,7 +1,7 @@
 from core.BaseTest import browser
 from pages.BasePage import BasePageHelper
 from pages.LoginPage import LoginPageHelper
-from pages.RegistrationPage import RegistrationPageHelperHelper
+from pages.RegistrationPage import RegistrationPageHelper
 import allure
 
 BASE_URL = 'https://ok.ru/'
@@ -12,7 +12,7 @@ def test_registration_random_country(browser):
     BasePageHelper(browser).get_url(BASE_URL)
     LoginPage = LoginPageHelper(browser)
     LoginPage.click_registration()
-    RegistrationPage = RegistrationPageHelperHelper(browser)
+    RegistrationPage = RegistrationPageHelper(browser)
     Select_country_code = RegistrationPage.select_random_country()
     Actual_country_code = RegistrationPage.get_phone_field_value()
     assert Select_country_code == Actual_country_code
